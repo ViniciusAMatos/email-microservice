23c068bee3d8df1b37113e265fe3d035c2012487
Vinicius André Matos Silva <vinicius.matos@seati.ma.gov.br>
2021-08-05 12:59:37 -0300

    Implementando RabbitMQ

--- src/main/java/br/vinicius/email/repositories/EmailRepository.java
+++ src/main/java/br/vinicius/email/repositories/EmailRepository.java
@@ -1,11 +1,13 @@
 package br.vinicius.email.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.vinicius.email.models.EmailModel;
 
 @Repository
-public interface EmailRepository extends JpaRepository<EmailModel, Long>{
+public interface EmailRepository extends JpaRepository<EmailModel, UUID>{
 
 }
